a93d4b3fb6cc753a42bfcf9ffd27f6e29aff8a57
VeryGoodYou <1984898516@qq.com>
2018-10-09 10:16:11 +0800

    完成

--- js/style.js
+++ js/style.js
@@ -19,4 +19,31 @@ $("#qq").modal({
 var mySwiper = new Swiper('.row-1',{
     freeMode : true,
     loop:true,
-})
+});
+/*控制视频关闭*/
+/*首页*/
+function out() {
+    var out = document.getElementsByTagName('video')[0];
+    out.load();
+}
+/*推荐*/
+function tui() {
+    var aa = document.getElementsByTagName('video')[1];
+    aa.load();
+}
+
+/*技术*/
+function ji() {
+    var aa = document.getElementsByTagName('video')[2];
+    aa.load();
+}
+/*赛事*/
+function sai() {
+    var aa = document.getElementsByTagName('video')[3];
+    aa.load();
+}
+/*娱乐*/
+function yu() {
+    var aa = document.getElementsByTagName('video')[4];
+    aa.load();
+}
